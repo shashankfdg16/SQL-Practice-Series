64a2a11e77ce974b961f0d8810c912d7feccf154
Shashank <shashankfdg72@gmail.com>
2025-08-13 15:30:51 +0530

    Create Task2-data_agg_report.sql

---
+++ Task2-data_agg_report.sql
@@ -0,0 +1,9 @@
+/*
+Data Aggregation & Reporting with Real Dataset
+----------------------------------------------
+This file contains SQL queries and validation checks for Task 2 of the 
+Employees Sample Database project. It covers advanced aggregation, 
+HAVING-based filtering, salary distribution reports, and validation queries. 
+Queries are provided in both multi-line (for readability) and one-line 
+formats (for CLI execution).
+*/
